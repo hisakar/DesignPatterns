81453d2a8246bbd26f42f3cfd438edad872d26a8
Orhan ŞAKAR <fakdilys@gmail.com>
2020-03-21 00:59:49 +0300

    implement singleton pattern

---
+++ src/DesignPatterns.SingletonPattern/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DesignPatterns.SingletonPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          //  var singleton = new Singleton();  ==>  Not work. Cause singleton has only private ctor
+
+            var singleton = Singleton.GetInstance("1"); // Now generated one instance of Singleton
+            var singleton2 = Singleton.GetInstance("2");
+            var singleton3 = Singleton.GetInstance("3");// Until here, all of references point the same object
+
+            //Proof
+            singleton.WriteText();
+            singleton2.WriteText();
+            singleton3.WriteText();
+        }
+    }
+}
---
+++ src/DesignPatterns.SingletonPattern/Singleton.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.SingletonPattern
+{
+    public class Singleton
+    {
+        private static Singleton singleton;
+        private static string _text;
+
+        private Singleton()
+        { }
+
+        public static Singleton GetInstance(string text)//Parameter are for proof
+        {
+            _text = text;
+            singleton = singleton ?? new Singleton();
+            return singleton;
+        }
+
+        public void WriteText()
+        {
+            Console.WriteLine(_text);
+        }
+
+    }
+}
